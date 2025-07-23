fef5f54b61b894a99e89d152c208d5b70722651d
Tarun Sai Srinivas <tarunsaisrinivas4@gmail.com>
2025-07-23 11:57:58 +0530

    23 jul class

--- 22_jul/speed.js
+++ 22_jul/speed.js
@@ -1,7 +1,7 @@
 console.log("Speed Conversion :");
 const KmHr = 90;
 const Ms = KmHr * (1000 / 3600);
-console.log(`${KmHr} km/h = ${Ms} m/s`);
+console.log(`${KmHr} km/h = ${Ms.toFixed(2)} m/s`);
 
 const MsToKmHr = Ms * (3600 / 1000);
 console.log(`${Ms} m/s = ${MsToKmHr} km/h`);

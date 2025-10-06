dcabbc0b2f799682d0902f01419bda387264b540
Tarun Sai Srinivas <tarunsaisrinivas4@gmail.com>
2025-10-06 14:52:36 +0530

    db setup done

---
+++ backend/project-four/lib/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      dbName: process.env.DB_NAME,
+    });
+    console.log("MongoDB connected to dbname:", mongoose.connection.db.name);
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  }
+};
+module.exports = { connectDB };
